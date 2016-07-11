f00587e23e8819d9226dfa4af791d7cdf8ef802f
Geoff Davis <geoffdavis92@gmail.com>
2016-07-11 12:33:10 -0500

    update styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,25 @@
-module.exports = {
+module.exports = function(grunt) {
   grunt.initConfig({
   	watch: {
-  		assets: {
+  		images: {
   			files: ['src/images/*'],
-  			tasks: ['copy:assets']
-  		}
+  			tasks: ['copy:images']
+  		},
+  		js: {
+  			files: ['src/es6/*.{es6,js}'],
+  			tasks: ['babel:dev']
+  		},
+  		js_lib: {
+  			files: ['src/es6/lib/*'],
+  			tasks: ['babel:dev_lib']
+  		},
+      sass: {
+        files: ['src/sass/**/*.sass'],
+        tasks: ['sass:dev']
+      }
   	},
   	copy: {
-  		tasks: {
+  		images: {
   			files: [{
   				expand: true,
   				cwd: 'src',
@@ -15,10 +27,52 @@ module.exports = {
   				dest: 'img'
   			}]
   		}
+  	},
+  	babel: {
+  		dev: {
+  			options: {
+  				sourceMap: true,
+  				presets: 'es2015'
+  			},
+  			files: [{
+  				expand: true,
+  				cwd: 'src/es6',
+  				src: '*.{es6,js}',
+  				dest: 'assets/js/',
+  				ext: '.js'
+  			}]
+  		},
+  		dev_lib: {
+  			options: {
+  				sourceMap: false,
+  				presets: 'es2015'
+  			},
+  			files: [{
+  				expand: true,
+  				cwd: 'src/es6/lib',
+  				src: '*',
+  				dest: 'assets/js/lib',
+  				ext: '.js'
+  			}]
+  		}
+  	},
+  	sass: {
+  		dev: {
+  			options: {},
+  			files: [{
+  				expand: true,
+  				cwd: 'src/sass/',
+  				src: 'main.sass',
+  				dest: 'assets/css/',
+  				ext: '.css'
+  			}]
+  		}
   	}
   })
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-babel');
 
   grunt.registerTask('default',['watch']);
 }
--- app.js
+++ app.js
@@ -4,18 +4,34 @@ const express = require('express'),
 	  server = require('./data/server/settings.js'),
 	  paths = require('./data/app/paths.js'),
 	  appSettings = require('./data/app/settings.js'),
+	  bodyParser = require('body-parser'),
+	  mysql      = require('mysql'),
+	  connection = mysql.createConnection({
+	  	host     : 'localhost',
+		user     : 'root',
+		password : 'root',
+		database : 'testdb'
+	  }),
 	  app = express();
 
 app
 	.set('views','views')
-	.set('view engine','pug');
+	.set('view engine','pug')
+	.use('/assets',express.static(__dirname + '/assets'))
+	.use(bodyParser.json())
+	.use(bodyParser.urlencoded({
+	  extended: true
+	}));
 
 app.listen(server.port, () => console.log(`Serving from ${server.port}`));
-
-
 
 for(let page in paths) {
 	app.get(paths[page].route, (req,res) => {
 		res.render(paths[page].file, {app:appSettings,page:paths[page]})
 	})
 }
+
+app.post('/action', (req,res) => {
+	console.log(req.body)
+	res.render('index')
+})
